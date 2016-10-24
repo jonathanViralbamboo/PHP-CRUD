589ce75fa7b1e41884f486931e403f3393cf87ab
jonathanViralbamboo <jonathan@viralbamboo.com>
2016-10-24 22:39:03 +0100

    Accepting User Data
    Filter the POSTED user data to make sure it's in the expected format

--- project_list.php
+++ project_list.php
@@ -6,34 +6,32 @@ $pageTitle = "Project List | Time Tracker";
 
 include 'inc/header.php';
 ?>
+
 <div class="section catalog random">
+  <div class="col-container page-container">
+    <div class="col col-70-md col-60-lg col-center">
+      <h1 class="actions-header">Project List</h1>
+      <div class="actions-item">
+        <a class="actions-link" href="project.php">
+          <span class="actions-icon">
+            <svg viewbox="0 0 64 64"><use xlink:href="#project_icon"></use></svg>
+          </span>
+          Add Project
+        </a>
+      </div>
+      <div class="form-container">
+        <ul class="items">
 
-    <div class="col-container page-container">
-        <div class="col col-70-md col-60-lg col-center">
-            <h1 class="actions-header">Project List</h1>
-            <div class="actions-item">
-                <a class="actions-link" href="project.php">
-                <span class="actions-icon">
-                  <svg viewbox="0 0 64 64"><use xlink:href="#project_icon"></use></svg>
-                </span>
-                Add Project
-                </a>
-            </div>
+          <?php
+          foreach (get_project_list() as $item) {
+            echo "<li>" . $item['title'] . "</li>";
+          }
+          ?>
 
-            <div class="form-container">
-                <ul class="items">
-
-                  <?php
-                  foreach (get_project_list() as $item) {
-                    echo "<li>" . $item['title'] . "</li>";
-                  }
-
-                  ?>
-                </ul>
-            </div>
-        </div>
+        </ul>
+      </div>
     </div>
-
+  </div>
 </div>
 
 <?php include("inc/footer.php"); ?>
