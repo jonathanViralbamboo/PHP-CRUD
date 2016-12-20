8b21efeaf64587b26f625513b5e17e58b4a296b1
jonathanViralbamboo <jonathan@viralbamboo.com>
2016-12-20 19:18:55 +0000

    Updating Projects
    Updating items from a database allows

--- inc/functions.php
+++ inc/functions.php
@@ -3,7 +3,7 @@
 
 function get_project_list() {
     include 'connection.php';
-
+    
     try {
         return $db->query('SELECT project_id, title, category FROM projects');
     } catch (Exception $e) {
@@ -14,31 +14,37 @@ function get_project_list() {
 
 function get_task_list($filter = null) {
     include 'connection.php';
-
+    
     $sql = 'SELECT tasks.*, projects.title as project FROM tasks'
         . ' JOIN projects ON tasks.project_id = projects.project_id';
-
+    
     $where = '';
     if (is_array($filter)) {
         switch ($filter[0]) {
-          case 'project':
-              $where = ' WHERE projects.project_id = ?';
-              break;
-          case 'category':
-              $where = ' WHERE category = ?';
-              break;
+            case 'project':
+                $where = ' WHERE projects.project_id = ?';
+                break;
+            case 'category':
+                $where = ' WHERE category = ?';
+                break;
+            case 'date':
+                $where = ' WHERE date >= ? AND date <= ?';
+                break;
         }
     }
-
+    
     $orderBy = ' ORDER BY date DESC';
     if ($filter) {
         $orderBy = ' ORDER BY projects.title ASC, date DESC';
     }
-
+    
     try {
         $results = $db->prepare($sql . $where . $orderBy);
         if (is_array($filter)) {
             $results->bindValue(1, $filter[1]);
+            if ($filter[0] == 'date') {
+                $results->bindValue(2, $filter[2],PDO::PARAM_STR);
+            }
         }
         $results->execute();
     } catch (Exception $e) {
@@ -48,15 +54,22 @@ function get_task_list($filter = null) {
     return $results->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function add_project($title, $category){
+function add_project($title, $category, $project_id = null){
     include 'connection.php';
-
-    $sql = 'INSERT INTO projects(title, category) VALUES(?, ?)';
-
+    
+    if ($project_id) {
+        $sql = 'UPDATE projects SET title = ?, category = ? WHERE project_id = ?';
+    } else {
+        $sql = 'INSERT INTO projects(title, category) VALUES(?, ?)';
+    }
+    
     try {
         $results = $db->prepare($sql);
         $results->bindValue(1, $title, PDO::PARAM_STR);
         $results->bindValue(2, $category, PDO::PARAM_STR);
+        if ($project_id) {
+            $results->bindValue(3, $project_id, PDO::PARAM_INT);
+        }
         $results->execute();
     } catch (Exception $e) {
         echo "Error!: " . $e->getMessage() . "<br />";
@@ -64,11 +77,27 @@ function add_project($title, $category){
     }
     return true;
 }
+
+function get_project($project_id){
+    include 'connection.php';
+    
+    $sql = 'SELECT * FROM projects WHERE project_id = ?';
+    
+    try {
+        $results = $db->prepare($sql);
+        $results->bindValue(1, $project_id, PDO::PARAM_INT);
+        $results->execute();
+    } catch (Exception $e) {
+        echo "Error!: " . $e->getMessage() . "<br />";
+        return false;
+    }
+    return $results->fetch();
+}
 function add_task($project_id, $title, $date, $time){
     include 'connection.php';
-
+    
     $sql = 'INSERT INTO tasks(project_id, title, date, time) VALUES(?, ?, ?, ?)';
-
+    
     try {
         $results = $db->prepare($sql);
         $results->bindValue(1, $project_id, PDO::PARAM_INT);
